571d2aa2903a2cd9efbe8fef45086b85f1e2db1c
robAnderson <reanderson89@gmail.com.com>
2023-04-25 07:18:36 -0700

    updated 04-01

--- 04-javascript/01-explain-what-you-see/script.js
+++ 04-javascript/01-explain-what-you-see/script.js
@@ -1,40 +1,42 @@
-    var userName = prompt("What's your name?");
+var userName = prompt("What's your name?");
 
-    alert("Welcome to our page " + userName);
-
-    var confirmSleep = confirm("Did you sleep last night?");
-
-    var sleepType = "bad";
-    var sleepHours = 5;
-    var wokeUpAtHome = true;
-
-    if (confirmSleep) {
-      alert("Hopefully it was good sleep" + " " + userName + "?");
-    }
-    else {
-      alert("So sorry to hear that" + " " + userName);
-    }
-
-    if (sleepType === "good") {
-      alert("It was good sleep!");
-    }
-    else {
-      alert("Looks like it could have been better");
-    }
-
-    if (sleepHours < 7) {
-      alert("You should try to get more sleep");
-    } else if (sleepHours > 10) {
-        alert("Oh no! my day is gone!")
-    } else {
-        alert("That was just right!")
-    }
-
-    if (wokeUpAtHome) {
-      alert("Phew...");
-    }
-    else {
-      alert("...not again!")
-    }
+if (userName === null){
+  alert("BOOOOOOOOO!!!")
+  userName = "No Name Given"
+} else {
+  alert("Welcome to our page " + userName);
+}
 
 
+
+var confirmSleep = confirm("Did you sleep last night?");
+
+if (confirmSleep) {
+  alert("Hopefully it was good sleep" + " " + userName + "?");
+  var sleepHours = prompt("How many hours did you sleep");
+
+  if (Number(sleepHours) < 7) {
+    alert("You should try to get more sleep");
+  } else if (sleepHours > 10) {
+    alert("Oh no! my day is gone!");
+  } else {
+    alert("That was just right!");
+  }
+
+  var sleepType = prompt("Was your sleep 'good' or 'bad'?");
+  if (sleepType === "good") {
+    alert("That's great!");
+  } else {
+    alert("Womp womp");
+  }
+
+  var wokeUpAtHome = confirm("Did you wake up at home?");
+
+  if (wokeUpAtHome) {
+    alert("Phew...");
+  } else {
+    alert("...not again!");
+  }
+} else {
+  alert("So sorry to hear that" + " " + userName);
+}
