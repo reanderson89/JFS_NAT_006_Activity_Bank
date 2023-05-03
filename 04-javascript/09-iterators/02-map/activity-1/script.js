3046d6c7ebf7cf73091ea52cae27827b9fd4fbe4
robAnderson <reanderson89@gmail.com.com>
2023-05-03 12:43:37 -0700

    added 09-iterators

---
+++ 04-javascript/09-iterators/02-map/activity-1/script.js
@@ -0,0 +1,8 @@
+// Activity 1: using the .map() iterator, iterate through the prices array and return a new array containing the new prices after applying a 20% discount.
+
+
+// your new array should look like this at the end: [80, 120, 160, 200]
+
+let prices = [100, 150, 200, 250]
+
+
---
+++ 04-javascript/09-iterators/04-reduce/activity-1/script.js
@@ -0,0 +1,6 @@
+// Activity: Use the .reduce() method to return the product of the given array ("product" in math is when you multiply two values together).
+
+// Example input: [1, 2, 3, 4, 5]
+// Example output: 120
+
+let nums = [1, 2, 3, 4, 5];
