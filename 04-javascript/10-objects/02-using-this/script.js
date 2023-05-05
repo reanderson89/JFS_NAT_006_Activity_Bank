0420b44f024b9743a17426841996a148cdd8eb51
robAnderson <reanderson89@gmail.com.com>
2023-05-05 08:55:49 -0700

    finished

--- 04-javascript/10-objects/02-using-this/script.js
+++ 04-javascript/10-objects/02-using-this/script.js
@@ -2,3 +2,14 @@
 // Create an object with 3 properties. 
 // Create a method in the object that "returns" a string that contains all 3 properties in a sentence.
 // print the return value of the method to the console
+
+const car = {
+    make: "ford",
+    model: "taurus",
+    year: 1998,
+    getInfo(){
+        return `This car is a ${this.year}, ${this.make} ${this.model}`
+    }
+}
+
+console.log(car.getInfo())
