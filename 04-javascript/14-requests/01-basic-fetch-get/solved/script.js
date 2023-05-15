211b3f8b980efacddb82cb368e4de0c4469c0350
robAnderson <reanderson89@gmail.com.com>
2023-05-15 13:01:10 -0700

    updated solved folder

--- 04-javascript/14-requests/01-basic-fetch-get/solved/script.js
+++ 04-javascript/14-requests/01-basic-fetch-get/solved/script.js
@@ -2,20 +2,23 @@
 
 // A FETCH REQUEST RETURNS A PROMISE
 
+// a fetch makes a GET request by default
+// endpoint: specifies a certain URL route in an application
 fetch("https://reqres.in/api/users")
 // handle initial response
 .then(response => {
     // the first .then() checks if the response is good
     console.log(response)
-    if(response.status === 200){
-        // response.json() gives us the data in a format we can work with
+    if(response.ok){
+        // response.json() gives us the body of the response in a format we can work with
         return response.json()
     } 
 })
 // we get access to the data in the next .then() statement
-.then( users => {
+.then( jsonResponse => {
     // here is where we can actually do something with the data for our application
-    console.log(users.data)
+    console.log(jsonResponse)
 })
 
 // .then() handles our promises
+
